57ac7190d23b812c5b16ee73363786b1d2fe7af3
Abderahmane BEHAR RAHALA <abderahmane.behar-rahala@c1r2s5.marseille.42.school>
2025-08-01 11:53:48 +0200

    42 School

---
+++ C_Simple_Strings/ex9/ft_putstr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abderahmane.behar-rahala <abderahmane.beh  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/01 11:13:52 by abderahmane.b     #+#    #+#             */
+/*   Updated: 2025/08/01 11:14:35 by abderahmane.b    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
